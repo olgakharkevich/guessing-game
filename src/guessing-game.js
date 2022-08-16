e39875bb9baae7a760dbce35cd168189453bf3f7
Olga Kharkevich <olga.kharkevich@gmail.com>
2022-08-16 16:29:02 -0400

    feat: add solution for guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.ceil((this.max - this.min) / 2 + this.min)
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
